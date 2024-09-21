47407c5104ac514da325f1e48645da6a4bc4de3f
Christian Medrano <christiandmc97@gmail.com>
2024-09-21 00:34:49 -0400

    Añadido mensaje de confirmación de conexión exitosa a la base de datos.

--- config/database.js
+++ config/database.js
@@ -1,10 +1,29 @@
 const { Sequelize } = require('sequelize');
 
+// Define las variables de configuración
+const dbName = 'bd_artesanias';       // Nombre de la base de datos
+const dbUser = 'postgres';       // Nombre de usuario
+const dbPassword = '12345678';
+const dbHost = 'localhost';      // Host de la base de datos
+
 // Configura la conexión a la base de datos
-const sequelize = new Sequelize('postgres', 'postgres', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
   dialect: 'postgres',
   logging: false,
 });
+
+// Función para verificar la conexión
+const verificarConexion = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión a la base de datos establecida con éxito.');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+};
+
+// Llama a la función para probar la conexión
+verificarConexion();
 
 module.exports = sequelize;
