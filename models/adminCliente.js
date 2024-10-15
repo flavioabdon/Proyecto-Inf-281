149e6af5adfdf0526b60f9c8190148694d2aa1bf
Christian Medrano <christiandmc97@gmail.com>
2024-10-15 00:45:22 -0400

    adminCliente: Se creo la funcion para actualizar en la parte de models.

--- models/adminCliente.js
+++ models/adminCliente.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/database'); // Importa la conexión a la base de datos
 
 // Función para listar todas las clientes
-const listarClientes = async () => {
+const listarAdminClientes = async () => {
     const query = `
     select *  from fn_listar_clientes()
     `;
@@ -19,6 +19,29 @@ const listarClientes = async () => {
 };
 
 
+// Función para actualizar un cliente
+const actualizarAdminCliente = async (id_usuario, ci, nombre, apellido, email, direccion, numero_contacto, sexo, estado) => {
+
+    const query = `
+    SELECT fn_actualizar_cliente($1, $2, $3, $4, $5, $6, $7, $8, $9);
+    `;
+    const values = [id_usuario, ci, nombre, apellido, email, direccion, numero_contacto, sexo, estado];
+
+    try {
+        const [res] = await sequelize.query(query, {
+            bind: values,
+            type: sequelize.QueryTypes.SELECT
+        });
+        return res; // Devuelve la categoría actualizada
+    } catch (error) {
+        console.error('Error al actualizar el cliente:', error);
+        throw error;
+    }
+};
+
+
+
 module.exports = {
-    listarClientes, 
+    listarAdminClientes,
+    actualizarAdminCliente,
 };
